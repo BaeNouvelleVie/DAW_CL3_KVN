8996015ea8928010e7fc9ff5eda00a0fd40fc073
BaeNouvelleVie <i202122429@cibertec.edu.pe>
2024-07-03 14:47:18 -0500

    Pregunta 2 Table Material Paginacion

--- src/app/pregunta02/pregunta02.component.ts
+++ src/app/pregunta02/pregunta02.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Character } from './Character';
+import { CharacterService } from '../character.service';
+import { MaterialModule } from '../angular-material/material/material.module';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta02',
-  standalone: true,
-  imports: [],
   templateUrl: './pregunta02.component.html',
-  styleUrl: './pregunta02.component.css'
+  styleUrls: ['./pregunta02.component.css'],
+  standalone: true,
+  imports: [MaterialModule,MatTableModule, MatPaginatorModule,NgClass]
 })
-export class Pregunta02Component {
+export class Pregunta02Component implements OnInit {
+  displayedColumns: string[] = ['name', 'status', 'gender', 'species'];
+  dataSource = new MatTableDataSource<Character>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private characterService: CharacterService) {}
+
+  ngOnInit(): void {
+    this.characterService.getHumanCharacters().subscribe((data) => {
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 }
